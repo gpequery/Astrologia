33d8c0ffc9ac86d7896e4dc3124fe0a5bdbbd020
gpequery <you@example.com>
2017-12-04 19:47:52 +0100

    init login without account

--- app/src/main/java/com/esgi/astrologia/Services/GoogleServices.java
+++ app/src/main/java/com/esgi/astrologia/Services/GoogleServices.java
@@ -1,12 +1,12 @@
 package com.esgi.astrologia.Services;
 
 import android.app.Activity;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 
-import com.google.android.gms.auth.api.Auth;
+import com.esgi.astrologia.SigninActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by Greg on 14/11/2017.
@@ -16,29 +16,29 @@ public class GoogleServices {
     private static GoogleServices googleService;
 
     private GoogleServices() {
-
     }
 
     public static GoogleServices getInstance() {
-        if(googleService == null) {
+        if (googleService == null) {
             googleService = new GoogleServices();
         }
 
         return googleService;
     }
 
-    public GoogleSignInOptions get_sign_in_options() {
+    private GoogleSignInOptions get_sign_in_options() {
         return new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
     }
 
-    public GoogleApiClient get_GoogleApiClient_builder(Activity activity) {
-        GoogleSignInOptions options = this.get_sign_in_options();
+    public GoogleSignInClient get_GoogleApiClient_builder(Activity activity) {
+        GoogleSignInOptions gso = this.get_sign_in_options();
 
-        return new GoogleApiClient.Builder(activity)
-                .enableAutoManage((FragmentActivity) activity, null )
-                .addApi(Auth.GOOGLE_SIGN_IN_API, options)
-                .build();
+        return GoogleSignIn.getClient(activity, gso);
+    }
+
+    public GoogleSignInAccount get_last_connection(SigninActivity activity) {
+        return GoogleSignIn.getLastSignedInAccount(activity);
     }
 }
--- app/src/main/java/com/esgi/astrologia/SigninActivity.java
+++ app/src/main/java/com/esgi/astrologia/SigninActivity.java
@@ -1,37 +1,23 @@
 package com.esgi.astrologia;
 
-import android.accounts.Account;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.DatePicker;
-
 import com.esgi.astrologia.Services.GoogleServices;
 import com.esgi.astrologia.Utils.User;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.plus.Plus;
-import com.google.android.gms.plus.model.people.Person;
-
 import java.util.Calendar;
 import java.util.Date;
 
-public class SigninActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
-    private GoogleApiClient mGoogleApiClient;
-    private SignInButton sign_in_with_google;
-    private int mYear;
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+    private GoogleSignInClient mGoogleApiClient;
+    /* Request code used to invoke sign in user interactions. */
+    private static final int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +27,7 @@ public class SigninActivity extends AppCompatActivity implements View.OnClickLis
         GoogleServices googleServices = GoogleServices.getInstance();
         mGoogleApiClient =  googleServices.get_GoogleApiClient_builder(this);
 
-        sign_in_with_google = (SignInButton) findViewById(R.id.sign_in_with_google);
-        sign_in_with_google.setSize(SignInButton.SIZE_WIDE);
+        SignInButton sign_in_with_google = findViewById(R.id.sign_in_with_google);
 
         sign_in_with_google.setOnClickListener(this);
     }
@@ -51,13 +36,35 @@ public class SigninActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.sign_in_with_google:
-                Log.i("MON_TAG", "Sign in with google");
+                sign_in_with_google();
                 break;
             case R.id.sign_in_without_account:
-                Log.i("MON_TAG", "Sign in without account");
                 sign_in_without_account();
                 break;
         }
+    }
+
+    private void sign_in_with_google() {
+        GoogleSignInAccount account = GoogleServices.getInstance().get_last_connection(this);
+
+        if (account == null) {
+            Log.i("MON_TAG", "account");
+            Intent signInIntent = mGoogleApiClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        } else {
+            String personName = account.getDisplayName();
+            String personGivenName = account.getGivenName();
+            String personFamilyName = account.getFamilyName();
+            String personEmail = account.getEmail();
+            String personId = account.getId();
+
+            Log.i("MON_TAG", "personName : " + personName);
+            Log.i("MON_TAG", "personGivenName : " + personGivenName);
+            Log.i("MON_TAG", "personFamilyName : " + personFamilyName);
+            Log.i("MON_TAG", "personEmail : " + personEmail);
+            Log.i("MON_TAG", "personId : " + personId);
+        }
+        //TO DO else goHome with current user
     }
 
     private void sign_in_without_account() {
@@ -66,25 +73,20 @@ public class SigninActivity extends AppCompatActivity implements View.OnClickLis
         int currentMonth = calendar.get(Calendar.MONTH);
         int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
 
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, SigninActivity.this, currentYear, currentMonth, currentDay);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, selectedYear, selectedMonth, selectedDay) -> {
+            Calendar calendar1 = Calendar.getInstance();
+            calendar1.set(Calendar.YEAR, selectedYear);
+            calendar1.set(Calendar.MONTH, selectedMonth);
+            calendar1.set(Calendar.DAY_OF_MONTH, selectedDay);
+
+            Date birthdate = calendar1.getTime();
+            User currentUser = new User(birthdate);
+
+            goHome(currentUser);
+        }, currentYear, currentMonth, currentDay);
+
+        datePickerDialog.getDatePicker().setMaxDate(calendar.getTimeInMillis());
         datePickerDialog.show();
-    }
-
-    @Override
-    public void onDateSet(DatePicker datePicker, int selectedYear, int selectedMonth, int selectedDay) {
-        Log.i("MON_TAG", "onDateSet");
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, selectedYear);
-        calendar.set(Calendar.MONTH, selectedMonth);
-        calendar.set(Calendar.DAY_OF_MONTH, selectedDay);
-
-        Date birthdate = calendar.getTime();
-        User currentUser = new User(birthdate);
-
-        goHome(currentUser);
-
-        Log.i("MON_TAG", "Date : " + calendar.get(Calendar.YEAR) + " / " + calendar.get(Calendar.MONTH) + " / " + calendar.get(Calendar.DAY_OF_MONTH));
     }
 
     private void goHome(User currentUser) {
@@ -92,36 +94,27 @@ public class SigninActivity extends AppCompatActivity implements View.OnClickLis
         startActivity(intent);
     }
 
-
-    private void signIn(View view) {
-        Log.i("MON_TAG", "vie");
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-
-        if (account == null) {
-            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-            startActivityForResult(signInIntent, RESULT_OK);
-        } else {
-            String personName = account.getDisplayName();
-            String personGivenName = account.getGivenName();
-            String personFamilyName = account.getFamilyName();
-            String personEmail = account.getEmail();
-            String personId = account.getId();
-            Uri personPhoto = account.getPhotoUrl();
-
-            Log.i("MON_TAG", "personName : " + personName);
-            Log.i("MON_TAG", "personGivenName : " + personGivenName);
-            Log.i("MON_TAG", "personFamilyName : " + personFamilyName);
-            Log.i("MON_TAG", "personEmail : " + personEmail);
-            Log.i("MON_TAG", "personId : " + personId);
-            Log.i("MON_TAG", "personPhoto : " + personPhoto);
-        }
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.i("MON_TAG", "onActivityResult");
+        Log.i("MON_TAG", "onActivityResult ");
+
+        if(resultCode == RESULT_OK) {
+            GoogleSignInAccount account = GoogleServices.getInstance().get_last_connection(this);
+
+            if(account != null) {
+                String personName = account.getDisplayName();
+                String personGivenName = account.getGivenName();
+                String personFamilyName = account.getFamilyName();
+                String personEmail = account.getEmail();
+                String personId = account.getId();
+
+                Log.i("MON_TAG", "personName : " + personName);
+                Log.i("MON_TAG", "personGivenName : " + personGivenName);
+                Log.i("MON_TAG", "personFamilyName : " + personFamilyName);
+                Log.i("MON_TAG", "personEmail : " + personEmail);
+                Log.i("MON_TAG", "personId : " + personId);
+            }
+        }
     }
-
-
 }
